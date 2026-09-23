a2bfd9d72acb3ea9cb09ff63bf4b085c7cc23cef
4dri4anxD <mauricio.uavsystems@gmail.com>
2026-09-23 08:59:56 -0700

    Swarm: add Spanish translation for formation/waypoint-leader controls

--- Swarm/FollowPathControl.cs
+++ Swarm/FollowPathControl.cs
@@ -34,7 +34,7 @@ namespace MissionPlanner.Swarm
             CMB_mavs.ValueMember = "Value";
             CMB_mavs.DisplayMember = "Key";
 
-            MessageBox.Show("this is beta, use at own risk");
+            MessageBox.Show(Strings.ThisIsBetaUseAtOwn);
 
             MissionPlanner.Utilities.Tracking.AddPage(this.GetType().ToString(), this.Text);
         }
@@ -136,7 +136,7 @@ namespace MissionPlanner.Swarm
 
                 if (DateTime.Now > deadline)
                 {
-                    CustomMessageBox.Show("Timeout waiting for autoscan/no mavlink device connected");
+                    CustomMessageBox.Show(Strings.Timeout);
                     return;
                 }
             }
